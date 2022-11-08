9fc848d0fe79cf32abbcd1adadcc30bd755ffdb2
Django-TOPS <djangotestmail2021@gmail.com>
2022-11-08 18:07:33 +0530

    Error Page Design

--- Django Project/NotesApp/NotesApp/settings.py
+++ Django Project/NotesApp/NotesApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%!#3d#ji0!-w20y1^xyp6o8)4%ihxy05m#m$g)go==-d**58#j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
